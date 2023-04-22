30f26e054bc68e273b4f84e58d461dccf85b7906
Josesan94 <josesantillan2@hotmai>
2023-04-22 00:55:42 -0300

    reorganice components

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,13 @@ import axios from "axios";
 
 const BASE_URL = "https://api.vatcomply.com";
 
-export const getRates = async (baseCurrency: string, wantedCurrency: string) => {
-  const response = await axios.get(`${BASE_URL}/exchange?base=${baseCurrency}&to=${wantedCurrency}`);
+export interface ratesObjectInterface {
+  base:  string;
+  date:  Date;
+  rates: { [key: string]: number };
+ }
+
+export const getRates = async (baseCurrency: string) => {
+  const response = await axios.get<ratesObjectInterface>(`${BASE_URL}/rates?base=${baseCurrency}`);
   return response.data;
 };
